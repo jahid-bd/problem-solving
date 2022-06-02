db9e163218dfe54a1d2475c14507b79819e398a1
jahid-bd <jahidbd9x@gmail.com>
2022-06-02 14:03:37 +0600

    hacker rank lonely integer problem solved

--- hacker-rank/1week-peparation/Day-2/lonely-integer.js
+++ hacker-rank/1week-peparation/Day-2/lonely-integer.js
@@ -1,62 +1,42 @@
-// function lonelyinteger(a) {
-//   // Write your code here
-//   /**
-//    * Start
-//    * step-1 = unique = a[0]
-//    * step-2 = a>loop
-//    * step-3 = if(unique == item) > unique = unique
-//    * step-4 = else > unique = item
-//    */
-//   let n = 0;
-//   let unique = a[n];
-//   let isDouble = false;
-
-//   // for (i + 1; i < a.length; i++) {
-//   //   if (unique === a[i]) {
-//   //     unique = a[i + 1];
-//   //     i = i + 1;
-//   //   }
-//   // }
-//   // console.log(isDouble);
-
-//   for (let i = 0; i < a.length; i++) {
-//     for (let j = i + 1; j < a.length; j++) {
-//       if (a[i] !== a[j]) {
-//         unique = a[i];
-//       } else {
-
-//       }
-//     }
-//   }
-
-//   console.log(unique);
-// }
+// Hacker rank 1week peparation day-2 lonely-integer problem
 
 function lonelyinteger(a) {
   // Write your code here
 
-  const arr = a.slice().short();
+  let count = 1;
 
-  let result = [];
+  const arrObj = a.reduce((acc, cur) => {
+    acc[cur] = 0;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] !== arr[i + 1]) {
-      result.push(arr[i]);
+    return acc;
+  }, {});
+
+  for (key in arrObj) {
+    a.map((item) => {
+      if (item == key) {
+        arrObj[key] = count++;
+      }
+    });
+    count = 1;
+  }
+
+  // let result = [];
+  let result = "";
+
+  for (key in arrObj) {
+    if (arrObj[key] < 2) {
+      // result.push(key);
+      result = key;
     }
   }
 
   // console.log(result);
+
+  return result;
 }
 
-// lonelyinteger([1, 2, 3, 4, 3, 1, 2, 3]);
+const lonelyItem = lonelyinteger([1, 2, 3, 4, 3, 1, 2, 3]);
 
-function findNonRepeating(arr, n) {
-  let res = 0;
-  for (let i = 0; i < n; i++) res = res ^ arr[i];
-  return res;
-}
+console.log(lonelyItem);
 
-const arr = [1, 2, 3, 4, 3, 1, 2, 3];
-let n = arr.length;
-
-console.log(findNonRepeating(arr, n));
+// node lonely-integer
